df5225ddb6ad75393ff9dcacbaef23f6483bd419
Group2-SWT <johanneberg2@gmail.com>
2021-10-28 15:49:25 +0200

    diagram + SQL scrip CC-database

---
+++ Database/CC-Database/CC-Database/Models/Bruger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC_Database.Models
+{
+    public class Bruger
+    {
+        [Key]
+        public int BrugerId { get; set; }
+        public string Navn { get; set; }
+        [MaxLength(30)]
+        public string Efternavn { get; set; }
+        [MaxLength(30)]
+        public string Foedselsdag { get; set; }
+        [MaxLength(30)]
+        public string Email { get; set; }
+        [MaxLength(30)]
+        public string Password { get; set; }
+
+    }
+}
---
+++ Database/CC-Database/CC-Database/data/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CC_Database.data
+{
+    public class MyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Relativ connection string
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CC-database;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
