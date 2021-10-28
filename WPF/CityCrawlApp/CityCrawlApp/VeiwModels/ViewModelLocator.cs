9f457bd60ae85c5f220d4c7bcaa01c10926ae47f
Asger <asger_bb@hotmail.com>
2021-10-28 14:39:22 +0200

    Startet på opsætning

---
+++ WPF/CityCrawlApp/CityCrawlApp/VeiwModels/ViewModelLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityCrawlApp.VeiwModels
+{
+    public class ViewModelLocator
+    {
+
+        public LoginViewModel ViewModel
+        {
+            get { return new LoginViewModel(); }
+        }
+    }
+}
