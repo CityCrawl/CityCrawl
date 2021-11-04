6c3e338882c8d78c29ced2b06a907b35237b3735
Maggi <78605132+au635196@users.noreply.github.com>
2021-11-04 15:19:20 +0100

    Start på binding af knapper

--- WPF/CityCrawlApp/CityCrawlApp/ViewModels/MainWindowViewModel.cs
+++ WPF/CityCrawlApp/CityCrawlApp/ViewModels/MainWindowViewModel.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prism.Commands;
+using Prism.Mvvm;
+using CityCrawlApp.Views;
+
 
 namespace CityCrawlApp.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BindableBase
     {
+        private DelegateCommand loginBtn;
+        public DelegateCommand LoginBtn =>
+            loginBtn ?? (loginBtn = new DelegateCommand(ExecuteLoginBtn));
 
+        void ExecuteLoginBtn()
+        {
+            var vmLogin = new LoginViewModel();
+            var dialog = new Login(vmLogin);
+
+            dialog.ShowDialog();
+        }
+
+        private DelegateCommand opretBrugerBtn;
+        public DelegateCommand OpretBrugerBtn =>
+            opretBrugerBtn ?? (opretBrugerBtn = new DelegateCommand(ExecuteOpretBrugerBtn));
+
+        void ExecuteOpretBrugerBtn()
+        {
+            var vmOpretBruger = new OpretBrugerViewModel();
+            var dialog = new OpretBruger(vmOpretBruger);
+
+            dialog.ShowDialog();
+        }
     }
 }
