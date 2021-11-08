b782a50ab5befb2167a9fd6c5d4a9494e8adc197
Group2-SWT <johanneberg2@gmail.com>
2021-11-08 08:27:00 +0100

    Updatering af CC_database
    I forhold til opdatering er ERD diagram

---
+++ Database/CC-Database/CC-Database/Models/CityCrawl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC_Database.Models
+{
+    public class CityCrawl
+    {
+        [Key]
+        public int CC_id { get; set; }
+        //Collection over Virksomheder
+        public ICollection<Virksomhed> Virksomhed { get; set; }
+        //Collection over Bruger
+        public ICollection<Bruger> Bruger { get; set; }
+    }
+}
