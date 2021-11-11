16a3ea4ab64ffd0a3e9b3a4bba95280f0c3c08ce
Asger <asger_bb@hotmail.com>
2021-11-11 15:41:34 +0100

    Tilmeld pubcrawl calender og pakke binding

--- WPF/CityCrawlApp/CityCrawlApp/ViewModels/TilmeldPubcrawlViewModel.cs
+++ WPF/CityCrawlApp/CityCrawlApp/ViewModels/TilmeldPubcrawlViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
+using CityCrawlApp.Models;
 using Prism.Mvvm;
 using Prism.Commands;
 using CityCrawlApp.Views;
@@ -12,9 +15,47 @@ namespace CityCrawlApp.ViewModels
 {
     public class TilmeldPubcrawlViewModel : BindableBase
     {
+        
+
         private string loggedInUser;
         private string userPassword;
 
+        private string selectedDate;
+        public string SelectedDate
+        {
+            get { return selectedDate; }
+            set { SetProperty(ref selectedDate, value); }
+        }
+
+        private DelegateCommand getPubCrawlOneAndDate;
+        public DelegateCommand GetPubCrawlOneAndDate =>
+            getPubCrawlOneAndDate ?? (getPubCrawlOneAndDate = new DelegateCommand(ExecuteCombineToListOne));
+
+        void ExecuteCombineToListOne()
+        {
+            User userInDB = new User();
+            userInDB.pubCrawls.Add("Pakke 1, " + selectedDate);
+
+            HttpClientPostUserToServerAsync(userInDB);
+
+            MessageBox.Show($"PubCrawl booket: {userInDB.pubCrawls.ElementAt(0)}");
+        }
+
+        
+
+        private DelegateCommand getPubCrawlTwoAndDate;
+        public DelegateCommand GetPubCrawlTwoAndDate =>
+            getPubCrawlTwoAndDate ?? (getPubCrawlTwoAndDate = new DelegateCommand(ExecuteCombineToListTwo));
+
+        void ExecuteCombineToListTwo()
+        {
+            User userInDB = new User();
+            userInDB.pubCrawls.Add("Pakke 2, " + selectedDate);
+
+            HttpClientPostUserToServerAsync(userInDB);
+
+            MessageBox.Show($"PubCrawl booket: {userInDB.pubCrawls.ElementAt(0)}");
+        }
 
         private DelegateCommand visProfil;
         public DelegateCommand VisProfil =>
@@ -35,6 +76,39 @@ namespace CityCrawlApp.ViewModels
             this.loggedInUser = loggedInUser;
             this.userPassword = userPassword;
             App.Current.MainWindow.Visibility = Visibility.Hidden;
+        }
+
+        private async Task HttpClientPostUserToServerAsync(User user)
+        {
+            using (var httpRequest = new HttpRequestMessage(HttpMethod.Post, Settings.baseUrl))
+            {
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                };
+
+                HttpClient client = new HttpClient();
+                //var content = new User()
+                //{
+                //    FirstName = user.FirstName,
+                //    LastName = user.LastName,
+                //    Birthday = user.Birthday,
+                //    Email = user.Email,
+                //    Password = user.Password
+                //};
+
+                var json = System.Text.Json.JsonSerializer.Serialize(user, options);
+                using (var stringContent = new StringContent(json, Encoding.UTF8, "application/json"))
+                {
+                    httpRequest.Content = stringContent;
+                    using (var httpResponse = await client
+                        .SendAsync(httpRequest, HttpCompletionOption.ResponseHeadersRead)
+                        .ConfigureAwait(false))
+                    {
+                        httpResponse.EnsureSuccessStatusCode();
+                    }
+                }
+            }
         }
     }
 }
