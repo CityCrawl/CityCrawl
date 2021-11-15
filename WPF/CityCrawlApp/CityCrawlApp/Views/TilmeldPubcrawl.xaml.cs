42e3a99c185df80a8efbaaa0b60bb68ee728460f
Maggi <78605132+au635196@users.noreply.github.com>
2021-11-15 13:22:17 +0100

    Ændring af MouseClick ved kalender

--- WPF/CityCrawlApp/CityCrawlApp/Views/TilmeldPubcrawl.xaml.cs
+++ WPF/CityCrawlApp/CityCrawlApp/Views/TilmeldPubcrawl.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -41,6 +42,15 @@ namespace CityCrawlApp.Views
         private void MenuItemShowProfile_OnClick(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        protected override void OnPreviewMouseUp(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseUp(e);
+            if(Mouse.Captured is CalendarItem)
+            {
+                Mouse.Capture(null);
+            }
         }
     }
  
