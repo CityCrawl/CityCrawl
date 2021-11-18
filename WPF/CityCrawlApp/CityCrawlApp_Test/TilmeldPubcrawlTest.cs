283e27b949bb5caabf182747844a44477ef1ce2c
Asger <asger_bb@hotmail.com>
2021-11-18 15:10:50 +0100

    TilmeldPubcrawlTest implementeret

---
+++ WPF/CityCrawlApp/CityCrawlApp_Test/TilmeldPubcrawlTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+using CityCrawlApp.ViewModels;
+using Prism.Commands;
+
+namespace CityCrawlApp.Test
+{
+    public class TilmeldPubcrawlTest
+    {
+        private TilmeldPubcrawlViewModel uut;
+        private string loggedInUser = "User@mail.dk";
+        private string userPassword = "testPassword";
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new TilmeldPubcrawlViewModel(loggedInUser, userPassword);
+        }
+
+        [Test]
+        public void TestGetPubcrawlOneAndDateDelegateCanExecute()
+        {
+            // Assert
+            uut.GetPubCrawlOneAndDate.CanExecute();
+        }
+
+        [Test]
+        public void TestGetPubcrawlTwoAndDateDelegateCanExecute()
+        {
+            // Assert
+            uut.GetPubCrawlTwoAndDate.CanExecute();
+        }
+
+        [Test]
+        public void TestVisProfilDelegateCanExecute()
+        {
+            // Assert
+            uut.VisProfil.CanExecute();
+        }
+    }
+}
