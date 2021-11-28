58ff774b6751aad9ac2d8bf7dc6bbec50046af7b
AndreasErslev <andreas.erslev@gmail.com>
2021-11-28 09:30:03 +0100

    Upload

---
+++ CC-Web/CC_Web_Andreas_27_11_21/NummerTre/CC-Web/CC-Web/Models/Data/CityCrawl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CC_Web.Models.Data
+{
+    public class CityCrawl
+    {
+        [Key]
+        public int CityCrawlID { get; set; }
+        public string Begivenhed { get; set; }
+        public string Sted { get; set; }
+        public string Dato { get; set; }
+        //Collection over Virksomheder, FK
+        //public List<Virksomhed> Virksomhed { get; set; }
+        //////Collection over Bruger, FK
+        //public List<Bruger> Bruger { get; set; }
+    }
+}
