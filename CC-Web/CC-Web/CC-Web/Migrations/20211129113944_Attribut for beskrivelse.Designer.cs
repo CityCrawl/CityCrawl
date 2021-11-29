0dfa6d76a62503a3c38cc754b28760d27ca9f54d
AndreasErslev <andreas.erslev@gmail.com>
2021-11-29 12:54:20 +0100

    Små ændringer

--- CC-Web/CC-Web/CC-Web/Migrations/20211129113944_Attribut for beskrivelse.Designer.cs
+++ CC-Web/CC-Web/CC-Web/Migrations/20211129113944_Attribut for beskrivelse.Designer.cs
@@ -4,14 +4,16 @@ using CC_Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CC_Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211129113944_Attribut for beskrivelse")]
+    partial class Attributforbeskrivelse
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
