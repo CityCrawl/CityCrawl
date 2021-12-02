7f0696a19d90758032603b2e731c4002cbfafae8
ChristinaPedersen <78605157+ChristinaPedersen@users.noreply.github.com>
2021-12-02 14:06:18 +0100

    tilføjelse af data objekt klassen Pubcrawl som skal sendes til Web API istedet for en string

---
+++ WPF/CityCrawlApp/CityCrawlApp/Models/Pubcrawl.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CityCrawlApp.Models
+{
+    public class Pubcrawl
+    {
+        public string PacketName { get; set; }
+        public string MeetDate { get; set; }
+        public string MeetPlace { get; set; }
+    }
+}
