d8d6f43a3ffaecd7c2189e38925a3a7be61f8b8a
Johanne <johanneberg2@gmail.com>
2021-12-02 15:29:20 +0100

    Endnu et nyt projekt, men lortet virker

---
+++ CC-Web/CC-Web_johanne_02_12_21/CC-Web/Controllers/AccountController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using static BCrypt.Net.BCrypt;
+using Microsoft.AspNetCore.Authorization;
+using CC_Web.Models.Data;
+using CC_Web.Data;
+using CC_Web.Utilities;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CC_Web.Controllers
+{ 
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        const int BcryptWorkfactor = 10;
+
+        private readonly ApplicationDbContext _context;
+        private readonly AppSettings _appSettings;
+        public AccountController(ApplicationDbContext context, IOptions<AppSettings> appSettings)
+        {
+            _context = context;
+            _appSettings = appSettings.Value;
+        }
+
+        [HttpPost("register"), AllowAnonymous]
+        public async Task<ActionResult<BrugerDto>> Register(BrugerDto regUser)
+        {
+            regUser.Email = regUser.Email.ToLower();
+            var emailExist = await _context.brugere.Where(u =>
+            u.Email == regUser.Email).FirstOrDefaultAsync();
+            if (emailExist != null)
+                return BadRequest(new { errorMessage = "Email already in use" });
+            Bruger user = new Bruger()
+            {
+                Email = regUser.Email,
+                Fornavn = regUser.Fornavn,
+                Efternavn = regUser.Efternavn,
+                Foedselsdag = regUser.Foedselsdag
+            };
+            user.PwHash = HashPassword(regUser.Password, BcryptWorkfactor);
+            _context.brugere.Add(user);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("Get", new { id = user.BrugerID }, GenerateToken(user));
+        }
+
+        // GET: api/Account/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<ActionResult<BrugerDto>> Get(int id)
+        {
+            var user = await _context.brugere.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var userDto = new BrugerDto();
+            userDto.Email = user.Email;
+            userDto.Fornavn = user.Fornavn;
+            userDto.Efternavn = user.Efternavn;
+            return userDto;
+        }
+
+        [HttpPost("login"), AllowAnonymous]
+        public async Task<ActionResult<TokenDto>> Login(BrugerDto login)
+        {
+            login.Email = login.Email.ToLower();
+            var user = await _context.brugere.Where(u => u.Email == login.Email)
+            .FirstOrDefaultAsync();
+            if (user != null)
+            {
+                var validPwd = Verify(login.Password, user.PwHash);
+                if (validPwd)
+                {
+                    var token = new TokenDto();
+                    token.JWT = GenerateToken(user);
+                    return token;
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Forkert brugernavn eller password");
+            return BadRequest(ModelState);
+        }
+        private string GenerateToken(Bruger user)
+        {
+            var claims = new Claim[]
+            {
+                new Claim("Email", user.Email),
+                new Claim(JwtRegisteredClaimNames.GivenName, user.Fornavn),
+                new Claim("BrugerId", user.BrugerID.ToString()),
+                new Claim(JwtRegisteredClaimNames.Exp,
+                new DateTimeOffset(DateTime.Now.AddDays(1)).ToUnixTimeSeconds().ToString()),
+            };
+            var key = Encoding.ASCII.GetBytes(_appSettings.SecretKey);
+            var token = new JwtSecurityToken(
+            new JwtHeader(new SigningCredentials(
+            new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)),
+            new JwtPayload(claims));
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
+
---
+++ CC-Web/CC-Web_johanne_02_12_21/CC-Web/Migrations/20211202114353_Changed DB. Pubcrawl added.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CC_Web.Migrations
+{
+    public partial class ChangedDBPubcrawladded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cityCrawls");
+
+            migrationBuilder.CreateTable(
+                name: "pubcrawls",
+                columns: table => new
+                {
+                    PubcrawlId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PakkeNavn = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pubcrawls", x => x.PubcrawlId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BrugerPubcrawl",
+                columns: table => new
+                {
+                    BrugereBrugerID = table.Column<int>(type: "int", nullable: false),
+                    PubcrawlsPubcrawlId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BrugerPubcrawl", x => new { x.BrugereBrugerID, x.PubcrawlsPubcrawlId });
+                    table.ForeignKey(
+                        name: "FK_BrugerPubcrawl_brugere_BrugereBrugerID",
+                        column: x => x.BrugereBrugerID,
+                        principalTable: "brugere",
+                        principalColumn: "BrugerID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BrugerPubcrawl_pubcrawls_PubcrawlsPubcrawlId",
+                        column: x => x.PubcrawlsPubcrawlId,
+                        principalTable: "pubcrawls",
+                        principalColumn: "PubcrawlId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PubcrawlVirksomhed",
+                columns: table => new
+                {
+                    PubcrawlsPubcrawlId = table.Column<int>(type: "int", nullable: false),
+                    VirksomhederVirksomhedID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PubcrawlVirksomhed", x => new { x.PubcrawlsPubcrawlId, x.VirksomhederVirksomhedID });
+                    table.ForeignKey(
+                        name: "FK_PubcrawlVirksomhed_pubcrawls_PubcrawlsPubcrawlId",
+                        column: x => x.PubcrawlsPubcrawlId,
+                        principalTable: "pubcrawls",
+                        principalColumn: "PubcrawlId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PubcrawlVirksomhed_virksomheder_VirksomhederVirksomhedID",
+                        column: x => x.VirksomhederVirksomhedID,
+                        principalTable: "virksomheder",
+                        principalColumn: "VirksomhedID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BrugerPubcrawl_PubcrawlsPubcrawlId",
+                table: "BrugerPubcrawl",
+                column: "PubcrawlsPubcrawlId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PubcrawlVirksomhed_VirksomhederVirksomhedID",
+                table: "PubcrawlVirksomhed",
+                column: "VirksomhederVirksomhedID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BrugerPubcrawl");
+
+            migrationBuilder.DropTable(
+                name: "PubcrawlVirksomhed");
+
+            migrationBuilder.DropTable(
+                name: "pubcrawls");
+
+            migrationBuilder.CreateTable(
+                name: "cityCrawls",
+                columns: table => new
+                {
+                    CityCrawlID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Begivenhed = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BrugerID = table.Column<int>(type: "int", nullable: true),
+                    Dato = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sted = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cityCrawls", x => x.CityCrawlID);
+                    table.ForeignKey(
+                        name: "FK_cityCrawls_brugere_BrugerID",
+                        column: x => x.BrugerID,
+                        principalTable: "brugere",
+                        principalColumn: "BrugerID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cityCrawls_BrugerID",
+                table: "cityCrawls",
+                column: "BrugerID");
+        }
+    }
+}
