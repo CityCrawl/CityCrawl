11d2b031118f0dbb9e634e0a9bee3f99f094f94d
Johanne <johanneberg2@gmail.com>
2021-12-09 13:27:16 +0100

    Seeddata

---
+++ CC-Web/CC-Web_Cookies/CC-Web/Data/SeedData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CC_Web.Data
+{
+    public static class SeedData
+    {
+        internal static void Seed(ApplicationDbContext context)
+        {
+
+        }
+
+        static void SeedCompany(ApplicationDbContext contaxt)
+        {
+
+        }
+    }
+}
