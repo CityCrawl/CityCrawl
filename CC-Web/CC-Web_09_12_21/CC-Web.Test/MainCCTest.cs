e92b441f8f0ded56d96a459513431f06ef2542f9
AndreasErslev <andreas.erslev@gmail.com>
2021-12-09 15:53:40 +0100

    Test filer

--- CC-Web/CC-Web_09_12_21/CC-Web.Test/MainCCTest.cs
+++ CC-Web/CC-Web_09_12_21/CC-Web.Test/MainCCTest.cs
@@ -1,36 +1,27 @@
 using CC_Web.Controllers;
 using CC_Web.Data;
 using CC_Web.Models.Data;
+using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
+using System.Threading.Tasks;
 
 namespace CC_Web.Test
 {
     public class Tests
     {
 
-        private readonly ApplicationDbContext _context;
-        MainCCController uut;
+        private ApplicationDbContext _context;
+        MainCCController _uut;
 
         [SetUp]
         public void Setup()
         {
-            var uut = new MainCCController(_context);
         }
 
         [Test]
         public void Test1()
         {
 
-            var vuser = new Virksomhed { CVR = "12345678", Virksomhedsnavn = "BarTest", Email = "bar@bartest.dk", Password = "1234", KontaktPerson = "Jan Test", Beskrivelse = "Dette er en bar der er lavet til at teste øl!"};
-
-            _context.Add(vuser);
-            _context.SaveChangesAsync();
-
-            var testProfil = uut.Profil("bar@bartest.dk");
-
-
-
-            Assert.IsTrue(testProfil.IsCompletedSuccessfully);
 
         }
     }
