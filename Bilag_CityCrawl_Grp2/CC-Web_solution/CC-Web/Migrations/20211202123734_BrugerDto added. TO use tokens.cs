a5e82f1fd8cd2eb213fcdf941fe19b47615ccbd3
AndreasErslev <andreas.erslev@gmail.com>
2021-12-13 13:12:09 +0100

    Upload af CC-Web
    Upload af færdig udgave af CC-Web solution.

---
+++ Bilag_CityCrawl_Grp2/CC-Web_solution/CC-Web/Migrations/20211202123734_BrugerDto added. TO use tokens.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CC_Web.Migrations
+{
+    public partial class BrugerDtoaddedTOusetokens : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "brugere",
+                newName: "PwHash");
+
+            migrationBuilder.RenameColumn(
+                name: "Foravn",
+                table: "brugere",
+                newName: "Fornavn");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MoedeSted",
+                table: "pubcrawls",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "MoedeTid",
+                table: "pubcrawls",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MoedeSted",
+                table: "pubcrawls");
+
+            migrationBuilder.DropColumn(
+                name: "MoedeTid",
+                table: "pubcrawls");
+
+            migrationBuilder.RenameColumn(
+                name: "PwHash",
+                table: "brugere",
+                newName: "Password");
+
+            migrationBuilder.RenameColumn(
+                name: "Fornavn",
+                table: "brugere",
+                newName: "Foravn");
+        }
+    }
+}
